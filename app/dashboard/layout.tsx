c6891c7f3fb8384d92657235b2d2f24648c63c16
Sauravssoni <85058209+Sauravssoni@users.noreply.github.com>
2026-08-26 19:39:14 +0530

    feat: expose programme office in dashboard shell

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,12 @@
 import DashboardBoot from "./DashboardBoot";
 import DashboardAlerts from "./DashboardAlerts";
+import DashboardProgramme from "./DashboardProgramme";
 import "./dashboard.css";
 import "./boot.css";
 import "./dashboard-alerts.css";
+import "./programme.css";
 import "./map-pro.css";
 
 export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <DashboardBoot>{children}<DashboardAlerts /></DashboardBoot>;
+  return <DashboardBoot>{children}<DashboardProgramme /><DashboardAlerts /></DashboardBoot>;
 }
